6335765fba5c396948d18e8b730c04b1a443d024
cechenique927 <58527176+cechenique927@users.noreply.github.com>
2020-12-06 12:03:27 -0500

    Create MergeSort.cpp

---
+++ MergeSort.cpp
@@ -0,0 +1,60 @@
+using namespace std;
+#include <vector>
+
+class Pixels{
+    private:
+    public:
+        void mergeSort(vector<Pixels> pixelVec, int left, int right);
+        void merge(vector<Pixels> pixelVec, int left, int mid, int right);
+};
+
+void Pixels::merge(vector<Pixels> pixelVec, int left, int mid, int right){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    Pixels X[n1];
+    Pixels Y[n2];
+
+    for(int i = 0; i < n1; i++){
+        X[i] = pixelVec[left+i];
+    }
+    for(int j = 0; j < n2; j++){
+        Y[j] = pixelVec[mid + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while ( i < n1 && j < n2){
+        if(X[i] <= Y[j]){
+            pixelVec[k] = X[i];
+            i++;
+        }
+        else{
+            pixelVec[k] = Y[j];
+            j++;
+        }
+        k++;
+    }
+    while ( i < n1){
+        pixelVec[k] = X[i];
+        i++;
+        k++;
+    }
+    
+    while ( j < n2){
+        pixelVec[k] = Y[j];
+        j++;
+        k++;
+    }
+}
+
+void Pixels::mergeSort(vector<Pixels> pixelVec, int left, int right){
+    if (left < right){
+        int mid = left +(right - left)/2;
+        mergeSort(pixelVec, left, mid);
+        mergeSort(pixelVec, mid + 1, right);
+        merge(pixelVec, left, mid, right);
+    }
+}
